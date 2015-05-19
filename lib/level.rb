1a1d169d5d6f99561695d25457d89cc293d354fd
Courtney <courtney.mae.phillips@gmail.com>
2015-05-19 14:43:41 -0700

    Refactor level_up method.

--- lib/level.rb
+++ lib/level.rb
@@ -1,16 +1,12 @@
 class Level < ActiveRecord::Base
   belongs_to :tamis
 
-  tami = Tami.find()
-  @@score = tami.score
-
-
   def level_up
 
     if @@score >= 100
       tami.update(level_id: 1)
     end
-    
+
     if @@score >= 200
       tami.update(level_id: 2)
     end
