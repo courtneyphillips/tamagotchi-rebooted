2f3248f2b77007c0c265558d285a92c9b01c57ef
Erin Collins <erincollinsthethird@gmail.com>
2015-05-22 10:33:04 -0700

    styling

--- lib/tama.rb
+++ lib/tama.rb
@@ -12,10 +12,10 @@ class Tama < ActiveRecord::Base
   end
 
   def start_update
-      self.update(food_level: (self.food_level-0))
-      self.update(activity_level: (self.activity_level-0))
-      self.update(sleep_level: (self.sleep_level-0))
-      self.update(drink_level: (self.drink_level-0))
+      self.update(food_level: (self.food_level-7))
+      self.update(activity_level: (self.activity_level-8))
+      self.update(sleep_level: (self.sleep_level-10))
+      self.update(drink_level: (self.drink_level-15))
   end
 
 
